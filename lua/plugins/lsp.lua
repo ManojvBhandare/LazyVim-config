b0384b5d247dff69017ee511947d84be693bbd2d
ManojvBhandare <manojbhandare.dev@gmail.com>
2024-01-13 13:11:30 +0530

    Config files uploaded

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,187 @@
+local cmp = require("cmp")
+return {
+	-- tools
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"stylua",
+				"selene",
+				"luacheck",
+				"shellcheck",
+				"shfmt",
+				"tailwindcss-language-server",
+				"typescript-language-server",
+				"css-lsp",
+			})
+		end,
+	},
+    
+
+	-- lsp servers
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			inlay_hints = { enabled = false },
+			---@type lspconfig.options
+			servers = {
+				cssls = {},
+				tailwindcss = {
+					root_dir = function(...)
+						return require("lspconfig.util").root_pattern(".git")(...)
+					end,
+				},
+				tsserver = {
+					root_dir = function(...)
+						return require("lspconfig.util").root_pattern(".git")(...)
+					end,
+					single_file_support = false,
+					settings = {
+						typescript = {
+							inlayHints = {
+								includeInlayParameterNameHints = "literal",
+								includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+								includeInlayFunctionParameterTypeHints = true,
+								includeInlayVariableTypeHints = false,
+								includeInlayPropertyDeclarationTypeHints = true,
+								includeInlayFunctionLikeReturnTypeHints = true,
+								includeInlayEnumMemberValueHints = true,
+							},
+						},
+						javascript = {
+							inlayHints = {
+								includeInlayParameterNameHints = "all",
+								includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+								includeInlayFunctionParameterTypeHints = true,
+								includeInlayVariableTypeHints = true,
+								includeInlayPropertyDeclarationTypeHints = true,
+								includeInlayFunctionLikeReturnTypeHints = true,
+								includeInlayEnumMemberValueHints = true,
+							},
+						},
+					},
+				},
+				html = {},
+				yamlls = {
+					settings = {
+						yaml = {
+							keyOrdering = false,
+						},
+					},
+				},
+				lua_ls = {
+					-- enabled = false,
+					single_file_support = true,
+					settings = {
+						Lua = {
+							workspace = {
+								checkThirdParty = false,
+							},
+							completion = {
+								workspaceWord = true,
+								callSnippet = "Both",
+							},
+							misc = {
+								parameters = {
+									-- "--log-level=trace",
+								},
+							},
+							hint = {
+								enable = true,
+								setType = false,
+								paramType = true,
+								paramName = "Disable",
+								semicolon = "Disable",
+								arrayIndex = "Disable",
+							},
+							doc = {
+								privateName = { "^_" },
+							},
+							type = {
+								castNumberToInteger = true,
+							},
+							diagnostics = {
+								disable = { "incomplete-signature-doc", "trailing-space" },
+								-- enable = false,
+								groupSeverity = {
+									strong = "Warning",
+									strict = "Warning",
+								},
+								groupFileStatus = {
+									["ambiguity"] = "Opened",
+									["await"] = "Opened",
+									["codestyle"] = "None",
+									["duplicate"] = "Opened",
+									["global"] = "Opened",
+									["luadoc"] = "Opened",
+									["redefined"] = "Opened",
+									["strict"] = "Opened",
+									["strong"] = "Opened",
+									["type-check"] = "Opened",
+									["unbalanced"] = "Opened",
+									["unused"] = "Opened",
+								},
+								unusedLocalExclude = { "_*" },
+							},
+							format = {
+								enable = false,
+								defaultConfig = {
+									indent_style = "space",
+									indent_size = "2",
+									continuation_indent_size = "2",
+								},
+							},
+						},
+					},
+				},
+			},
+			setup = {
+				
+					    
+            },
+		},
+	},
+	{
+		"hrsh7th/cmp-nvim-lsp"
+	  },
+	  {
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+		  "saadparwaiz1/cmp_luasnip",
+		  "rafamadriz/friendly-snippets",
+		},
+	  },
+	  {
+		"hrsh7th/nvim-cmp",
+		config = function()
+		  local cmp = require("cmp")
+		  require("luasnip.loaders.from_vscode").lazy_load()
+	
+		  cmp.setup({
+			snippet = {
+			  expand = function(args)
+				require("luasnip").lsp_expand(args.body)
+			  end,
+			},
+			window = {
+			  completion = cmp.config.window.bordered(),
+			  documentation = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert({
+			  ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			  ["<C-f>"] = cmp.mapping.scroll_docs(4),
+			  ["<C-Space>"] = cmp.mapping.complete(),
+			  ["<C-e>"] = cmp.mapping.abort(),
+			  ["<CR>"] = cmp.mapping.confirm({ select = true }),
+			}),
+			sources = cmp.config.sources({
+			  { name = "nvim_lsp" },
+			  { name = "luasnip" }, -- For luasnip users.
+			}, {
+			  { name = "buffer" },
+			}),
+		  })
+		end,
+	  },
+	
+}
